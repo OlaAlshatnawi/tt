75faf1a6d4519a961e94229e145888a9c312da72
Your Name <you@example.com>
2023-08-07 23:59:00 +0300

    t

---
+++ TestCI2-master/TestCI2-master/src/main/java/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main");
+        System.out.println(cubeX(5));
+        System.out.println(squareX(5));
+        System.out.println("A new modification from IntelliJ");
+
+        System.out.println("A new modification from saif");
+
+
+ System.out.println("moath's modification");
+
+
+    }
+   
+    public static int cubeX(int x){
+        return x * x  ;
+    }
+
+    public static int squareX(int x){
+        return x * x ;
+    }
+}
---
+++ TestCI2-master/TestCI2-master/src/test/java/TestCases.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+public class TestCases {
+    @Test
+    public void test1(){
+        assertEquals(27, Main.cubeX(3));
+    }
+
+    @Test
+    public void test2(){
+        assertEquals(9, Main.squareX(3));
+    }
+}
